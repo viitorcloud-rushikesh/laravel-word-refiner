9cf31ed1117cff407e798b9121b30db360e37f2e
viitorcloud-rushikesh <rushikesh.soni@viitorcloud.co>
2019-01-16 19:19:22 +0530

    Maintain to allow to check if input has obscene words or not

--- src/http/helpers.php
+++ src/http/helpers.php
@@ -3,40 +3,69 @@
 if (!function_exists('refiner')) {
     /**
      * @param $request
+     * @param bool $inputsHasObsceneWords
+     *
      * @return \Illuminate\Support\Collection
      */
-    function refiner($request)
+    function refiner($request, $inputsHasObsceneWords = false)
     {
         try {
+            // Get all requested inputs
             $requestData = $request->all();
-            if ($requestData) {
-                foreach ($requestData as $individualKey => $individualValue) {
-                    $filterString = [];
-                    $requestedString = [];
+            $containObsceneWords = false;
 
-                    if (!is_array($individualValue)) {
-                        $requestedString = strtolower($individualValue);
-                        $requestedString = explode(' ', $requestedString);
-                    }
+            if (config('refiner')) { // Check refiner config file is published or not
 
-                    $detectWords = config('refiner');
-                    $detectWords = array_flip($detectWords);
+                // Get all obscene words from the config file
+                $detectWords = config('refiner');
+                $detectWords = array_flip($detectWords);
 
-                    foreach ($requestedString as $words) {
-                        if (!isset($detectWords[$words])) {
-                            $filterString[] = $words;
+                if ($requestData) { // Check request has inputs or not
+                    foreach ($requestData as $individualKey => $individualValue) { // Individually check all input string
+                        $filterString = [];
+                        $requestedString = [];
+
+                        if (!is_array($individualValue)) { // Convert input value into an array by space
+                            $requestedString = strtolower($individualValue);
+                            $requestedString = explode(' ', $requestedString);
+                        }
+
+                        foreach ($requestedString as $words) { // Check individual input's words, is they contain obscene word or not
+                            if ($inputsHasObsceneWords == false) { // Set words into new variable except for obscene words if a request for inputs removing obscene words
+                                if (!isset($detectWords[$words])) {
+                                    $filterString[] = $words;
+                                }
+                            }
+
+                            if ($inputsHasObsceneWords == true) { // Set return flag if a request to check inputs has obscene words
+                                if (isset($detectWords[$words])) {
+                                    $containObsceneWords = true;
+                                }
+                            }
+                        }
+
+                        if ($inputsHasObsceneWords == false) { // Update input value without obscene words if a request for inputs removing obscene words
+                            $filterString = implode(' ', $filterString);
+                            $request = $request->merge([$individualKey => $filterString]);
                         }
                     }
-
-                    $filterString = implode(' ', $filterString);
-                    $request = $request->merge([$individualKey => $filterString]);
                 }
             }
 
-            return $request;
+            if ($inputsHasObsceneWords == false) { // Set response base on the request type
+                return $request;
+            } else {
+                return $containObsceneWords;
+            }
         } catch (\Exception $ex) {
+            // Generate a log message if any error caught
             \Log::error($ex->getMessage());
-            return collect([]);
+
+            if ($inputsHasObsceneWords == false) { // Set response base on the request type
+                return collect([]);
+            } else {
+                return false;
+            }
         }
     }
 }
